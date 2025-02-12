d0723ee042fd2af181e2e9c056be7b44aab96a2a
Orbán Levente <orban.levente.laszlo@gmail.com>
2025-02-12 17:37:07 +0100

    (docs 1/12): Finish first lecture notes.

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import re
 def define_env(env):
     pass
 
-def add_quotes(text : str) -> str:
+def _add_quotes(text : str) -> str:
     if not text:
         return ''
     
@@ -18,7 +18,17 @@ def compatible_admonition(text : str) -> str:
     '''
 
     pattern = r'(!!!|\?\?\?\+?)\s*(note|abstract|info|tip|success|question|warning|faliure|danger|bug|example|quote)[^\S\r\n](.*)?'
-    return re.sub(pattern, lambda m: f'{m.group(1)} {m.group(2)} {add_quotes(m.group(3))}', text, flags=re.IGNORECASE)
+    return re.sub(pattern, lambda m: f'{m.group(1)} {m.group(2)} {_add_quotes(m.group(3))}', text, flags=re.IGNORECASE)
+
+def katex_block_linebreaks(text : str) -> str:
+    '''
+    This function adds a line break before and after every KaTeX block.
+    '''
+
+    before_pattern = r'(?!\n)(\s*\$\$.*?\$\$)'
+    after_pattern = r'(\s*\$\$.*?\$\$)(?!\n)'
+    return re.sub(before_pattern, r'\n\1\n', re.sub(after_pattern, r'\n\1\n', text))
+
 
 def on_pre_page_macros(env):
     '''
@@ -26,6 +36,9 @@ def on_pre_page_macros(env):
     '''
     text = compatible_admonition(env.markdown)
     
+    text = katex_block_linebreaks(text)
+
     # * ADD YOUR PREPROCESSING HERE
+    # Use https://regexr.com/ to understand the regex patterns
 
     env.markdown = text

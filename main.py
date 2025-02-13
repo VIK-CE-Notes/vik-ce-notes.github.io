62aea2a18ccde6f5c7a4273e2d3c50c551e426aa
Horváth Kristóf <hk20021084@gmail.com>
2025-02-13 17:42:13 +0100

    Improving urls

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import re
+from mkdocs_macros.plugin import MacrosPlugin
+from unidecode import unidecode
 
 def define_env(env):
     pass
@@ -20,11 +22,21 @@ def compatible_admonition(text : str) -> str:
     pattern = r'(!!!|\?\?\?\+?)\s*(note|abstract|info|tip|success|question|warning|faliure|danger|bug|example|quote)[^\S\r\n](.*)?'
     return re.sub(pattern, lambda m: f'{m.group(1)} {m.group(2)} {_add_quotes(m.group(3))}', text, flags=re.IGNORECASE)
 
-def on_pre_page_macros(env):
+def remove_url_accents(text : str) -> str:
+    '''
+    Cause the headings get the id without the accents
+    '''
+
+    pattern = r'\[(.*)\]\((.*)\)'
+    return re.sub(pattern, lambda m: f'[{m.group(1)}]({unidecode(m.group(2))})', text)
+
+
+def on_pre_page_macros(env : MacrosPlugin):
     '''
     This function is called before the page macros are processed.
     '''
     text = compatible_admonition(env.markdown)
+    text = remove_url_accents(text)
     
     # * ADD YOUR PREPROCESSING HERE
     # Use https://regexr.com/ to understand the regex patterns

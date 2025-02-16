a8311afdeebb6e5fe59d524d5f7540b056ac05f4
Gilgames <balintkovacs11@gmail.com>
2025-02-16 23:17:56 +0100

    katex linebreaks preprocessing

--- main.py
+++ main.py
@@ -20,12 +20,24 @@ def compatible_admonition(text : str) -> str:
     pattern = r'(!!!|\?\?\?\+?)\s*(note|abstract|info|tip|success|question|warning|faliure|danger|bug|example|quote)[^\S\r\n](.*)?'
     return re.sub(pattern, lambda m: f'{m.group(1)} {m.group(2)} {_add_quotes(m.group(3))}', text, flags=re.IGNORECASE)
 
+def katex_block_linebreaks(text : str) -> str:
+    '''
+    This function adds a line break before and after every KaTeX block.
+    '''
+
+    before_pattern = r'(?!\n)(\$\$.*?\$\$)'
+    after_pattern = r'(\$\$.*?\$\$)(?!\n)'
+    return re.sub(before_pattern, r'\n\1\n', re.sub(after_pattern, r'\n\1\n', text))
+
+
 def on_pre_page_macros(env):
     '''
     This function is called before the page macros are processed.
     '''
     text = compatible_admonition(env.markdown)
     
+    text = katex_block_linebreaks(text)
+
     # * ADD YOUR PREPROCESSING HERE
     # Use https://regexr.com/ to understand the regex patterns
 
